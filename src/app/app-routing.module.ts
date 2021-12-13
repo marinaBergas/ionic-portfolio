a2ec345c11de7df453f0ff70441c32eb14fe4ae1
marinaBergas <marina.bergas@gmail.com>
2021-12-13 18:54:02 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,6 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  { path: 'portfolio/:id', component: ProjectDetailsComponent },
 
   { path: 'about', component: AboutComponent },
   { path: 'sign-in',
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectDetailsComponent } from './components/project-details/project-details.component';
 
 import { HomePage } from './home.page';
 
@@ -7,7 +8,8 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-
+    children:[  { path: 'portfolio/:id', component: ProjectDetailsComponent },
+  ]
   }
 
 ];
